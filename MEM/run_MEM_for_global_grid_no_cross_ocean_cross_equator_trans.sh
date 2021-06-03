5b53d4826a5bc1a28cb8dd31590c9b428c8d7602
d-farnham <dfarnham@carnegiescience.edu>
2021-06-03 13:41:25 -0400

    Add new MEM runs that exclude cross-oceanic and cross-equator transmission lines

---
+++ MEM/run_MEM_for_global_grid_no_cross_ocean_cross_equator_trans.sh
@@ -0,0 +1,12 @@
+
+#############################################################
+######################## Regular run ########################
+#############################################################
+# 2016
+python Run_Case_Example_default.py case_inputs/global_grid/global_grid_connected_five_storage_no_cross_ocean_cross_equator_trans_lines_2016.csv
+
+# 2017
+python Run_Case_Example_default.py case_inputs/global_grid/global_grid_connected_five_storage_no_cross_ocean_cross_equator_trans_lines_2017.csv
+
+# 2018
+python Run_Case_Example_default.py case_inputs/global_grid/global_grid_connected_five_storage_no_cross_ocean_cross_equator_trans_lines_2018.csv
